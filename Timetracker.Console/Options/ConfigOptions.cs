23e957486a0e74117d3cc0164ba707e75c7df441
Jonas Lima de Amorim <dev.amorim@outlook.com>
2025-02-17 13:04:25 -0300

    fix: changing the base url format

--- Timetracker.Console/Options/ConfigOptions.cs
+++ Timetracker.Console/Options/ConfigOptions.cs
@@ -5,7 +5,7 @@ namespace Timetracker.Options;
 [Verb("config", HelpText = "Configure the CLI parameters for connecting to your Timetracker instance.")]
 public class ConfigOptions
 {
-    [Option('u', "url", Required = true, HelpText = "Specify the URL for your Timetracker instance (e.g., https://<company>.timehub.7pace.com/api/odata).")]
+    [Option('u', "url", Required = true, HelpText = "Specify the URL for your Timetracker instance (e.g., https://<company>.timehub.7pace.com).")]
     public string TimetrackerUrl { get; set; }
 
     [Option('t', "token", Required = true, HelpText = "Provide the Bearer Token required for authentication with your Timetracker instance.")]
